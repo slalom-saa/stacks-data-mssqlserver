8d66a5266b0d08e6a36e648caa53a488ff8a6840
George Olson <george.olson@windowslive.com>
2017-03-11 07:04:07 -0800

    Final version commits.

--- src/Slalom.Stacks.Logging.SqlServer/SqlLogger.cs
+++ src/Slalom.Stacks.Logging.SqlServer/SqlLogger.cs
@@ -7,10 +7,45 @@ using Serilog.Sinks.MSSqlServer;
 using Slalom.Stacks.Runtime;
 using Slalom.Stacks.Validation;
 using Serilog.Context;
+using Serilog.Events;
 using Slalom.Stacks.Messaging;
+using Environment = Slalom.Stacks.Runtime.Environment;
 
 namespace Slalom.Stacks.Logging.SqlServer
 {
+    public class LogEventEnricher : ILogEventEnricher
+    {
+        private readonly ExecutionContext _context;
+        private Environment _environment;
+
+        public LogEventEnricher(object[] context)
+        {
+            _context = context.OfType<ExecutionContext>().FirstOrDefault();
+            _environment = context.OfType<Environment>().FirstOrDefault();
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (_context != null)
+            {
+                if (_context.Request.User != null && _context.Request.User.Identity.IsAuthenticated)
+                {
+                    logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserName", _context.Request.User.Identity.Name));
+                }
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("CorrelationId", new ScalarValue(_context.Request.CorrelationId)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("SourceAddress", new ScalarValue(_context.Request.SourceAddress)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("SessionId", new ScalarValue(_context.Request.SessionId)));
+            }
+            if (_environment != null)
+            {
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("ApplicationName", new ScalarValue(_environment.ApplicationName)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("EnvironmentName", new ScalarValue(_environment.EnvironmentName)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("MachineName", new ScalarValue(_environment.MachineName)));
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("ThreadId", new ScalarValue(_environment.ThreadId)));
+            }
+        }
+    }
+
     public class SqlLogger : ILogger
     {
         private readonly Logger _logger;
@@ -71,15 +106,7 @@ namespace Slalom.Stacks.Logging.SqlServer
         /// <param name="properties">Objects positionally formatted into the message template.</param>
         public void Error(Exception exception, string template, params object[] properties)
         {
-            var context = properties.OfType<ExecutionContext>().FirstOrDefault();
-            if (context != null)
-            {
-                using (var log = LogContext.PushProperty("Context", context, true))
-                {
-                    _logger.Error(exception, template, properties);
-                }
-            }
-            else
+            using (LogContext.PushProperties(new LogEventEnricher(properties)))
             {
                 _logger.Error(exception, template, properties);
             }
--- src/Slalom.Stacks.Logging.SqlServer/SqlServerSink.cs
+++ src/Slalom.Stacks.Logging.SqlServer/SqlServerSink.cs
@@ -337,7 +337,7 @@ namespace Slalom.Stacks.Logging.SqlServer
             {
                 DataType = typeof(string)
             });
-            eventsTable.Columns.Add(new DataColumn("Environment")
+            eventsTable.Columns.Add(new DataColumn("EnvironmentName")
             {
                 DataType = typeof(string)
             });
@@ -368,7 +368,7 @@ namespace Slalom.Stacks.Logging.SqlServer
             {
                 DataType = typeof(string),
                 MaxLength = -1,
-                ColumnName = "Session"
+                ColumnName = "SessionId"
             });
 
             // Create an array for DataColumn objects.
@@ -421,15 +421,27 @@ namespace Slalom.Stacks.Logging.SqlServer
                     this.ConvertPropertiesToColumn(row, logEvent.Properties);
                 }
 
-                //row["ApplicationName"] = this.Context.ApplicationName;
-                //row["Environment"] = this.Context.Environment;
-                //row["ThreadId"] = this.Context.ThreadId;
-                //row["MachineName"] = this.Context.MachineName;
+                SetColumnValue(logEvent, row, "UserName");
+                SetColumnValue(logEvent, row, "CorrelationId");
+                SetColumnValue(logEvent, row, "SourceAddress");
+                SetColumnValue(logEvent, row, "ApplicationName");
+                SetColumnValue(logEvent, row, "EnvironmentName");
+                SetColumnValue(logEvent, row, "MachineName");
+                SetColumnValue(logEvent, row, "ThreadId");
+                SetColumnValue(logEvent, row, "SessionId");
 
                 _eventsTable.Rows.Add(row);
             }
 
             _eventsTable.AcceptChanges();
+        }
+
+        private static void SetColumnValue(LogEvent logEvent, DataRow row, string name)
+        {
+            if (logEvent.Properties.Keys.Contains(name))
+            {
+                row[name] = ((ScalarValue)logEvent.Properties[name]).Value;
+            }
         }
 
         private string LogEventToJson(LogEvent logEvent)
